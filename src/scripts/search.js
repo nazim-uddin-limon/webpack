bfc12e2a748e1ffca6f6e2339ca4c4a3d95d1d5b
Nazim Uddin <102810077+nazim-uddin-limon@users.noreply.github.com>
2022-05-17 22:48:38 +0600

    Add files via upload

---
+++ src/scripts/search.js
@@ -0,0 +1,11 @@
+export const fns = () => {
+    console.log('I am function from search')
+}
+
+export const arr = [1, 3, 4, 5, 6, 7, 8, 9]
+
+export const printArr = (array) => {
+    for(let i = 0; i < array.length; i++){
+        console.log(array[i])
+    }
+}
